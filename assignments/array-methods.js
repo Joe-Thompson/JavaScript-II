1dcc93738a9d94fdb15396b25d07850fe171680f
joe.thompson3696 <bmo3696@gmail.com>
2019-07-09 22:12:42 -0400

    updated challenge 2 to use .map and challenge 3 to use actually use the filter method

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,22 +66,20 @@ console.log(fullName);
 
 // ==== Challenge 2: Use 
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+// let allCaps = [];
 
-runners.forEach((item) => {
- allCaps.push(item.first_name.toUpperCase());
- return(allCaps);
-})
+let allCaps = runners.map((item) => {
+  return item.first_name.toUpperCase();
+});
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+// let largeShirts = [];
 
-runners.filter((item) => {
-  if (item.shirt_size === "L") {
-    largeShirts.push(item.first_name + " " + item.last_name);
-  }
+let largeShirts = runners.filter((item) => {
+  return item.shirt_size === 'L';
 });
 
 console.log(largeShirts);
